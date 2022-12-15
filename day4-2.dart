e10c420d0bd8fdf7109d698d03b4c81227e6d867
Jacob Duba <jacob@mooshoe.me>
2022-12-14 19:27:33 -0600

    day5

---
+++ day4-2.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main() {
+  int count = 0;
+
+  while (true) {
+    String input = stdin.readLineSync()!;
+
+    if (input == "") {
+      print(count);
+      break;
+    }
+
+    final List<String> split = input.split(",");
+    final List<int> alpha = split[0].split("-").map((e) => int.parse(e)).toList();
+    final List<int> beta = split[1].split("-").map((e) => int.parse(e)).toList();
+
+    if (alpha[1] <= beta[0] && alpha[0] >= beta[1]) {
+      count++;
+    } else if (alpha[1] >= beta[0] && alpha[0] <= beta[1]) {
+      count++;
+    }
+  }
+}
---
+++ day5-1.dart
@@ -0,0 +1,53 @@
+import 'dart:collection';
+import 'dart:io';
+
+void main() async {
+  List<String> content = await File('day5.txt').readAsLinesSync();
+
+  int i = 0;
+  while (content[i] != "") {
+    i++;
+  }
+
+  List<String> stacksIn = content.sublist(0, i);
+  Iterable<List<String>> instructions = content.sublist(i + 1).map((e) => e.split(" ").toList());
+
+  List<Queue<String>> stacks = generateStacks(stacksIn);
+  moveStacks(stacks, instructions);
+
+  for (Queue<String> stack in stacks) {
+    stdout.write(stack.first);
+  }
+}
+
+List<Queue<String>> generateStacks(List<String> input) {
+  List<Queue<String>> stacks = List.empty(growable: true);
+  int i = 1;
+  while (i < input[0].length) {
+    ListQueue<String> stack = new ListQueue<String>();
+    
+    int j = input.length - 2;
+
+    while (j != -1 && input[j][i] != " ") {
+      stack.addFirst(input[j][i]);
+      j--;
+    }
+
+    stacks.add(stack);
+    i += 4;
+  }
+  return stacks;
+}
+
+void moveStacks(List<Queue<String>> stacks, Iterable<List<String>> instructions) {
+  for (List<String> instruction in instructions) {
+    int amount = int.parse(instruction[1]); 
+    int from = int.parse(instruction[3]) - 1; 
+    int to = int.parse(instruction[5]) - 1;
+
+    for (int i = 0; i < amount; i++) {
+      stacks[to].addFirst(stacks[from].removeFirst());
+    }
+  }
+}
+
